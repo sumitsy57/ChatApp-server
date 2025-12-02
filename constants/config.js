717d0dff77bba2a66449763e070c8c32ea127f9b
unknown <sumityadav2428@gmail.com>
2025-12-02 20:38:10 +0530

    issue done

--- constants/config.js
+++ constants/config.js
@@ -1,22 +1,13 @@
-const CLIENT_ORIGINS = [
-  "http://localhost:5173",
-  "http://localhost:4173",
-  process.env.CLIENT_URL,
-].filter(Boolean);
-
-export const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (CLIENT_ORIGINS.indexOf(origin) !== -1) return callback(null, true);
-    return callback(new Error("CORS: Origin not allowed - " + origin));
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-    "Accept",
-    "Origin",
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
   ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 };
+
+const CHATTU_TOKEN = "chattu-token";
+
+export { corsOptions, CHATTU_TOKEN };
